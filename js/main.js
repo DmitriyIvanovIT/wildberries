2f2402a12874075b37ba97d3a960ea762422813f
Anastasia Gartseva <amgartseva@gmail.com>
2021-03-26 12:31:30 +0300

    functions js

--- js/main.js
+++ js/main.js
@@ -73,3 +73,96 @@ scrollLink.forEach(item => {
 		scrolling(e, item);
 	})
 });
+
+// goods
+
+const more = document.querySelector('.more'),
+	navigationLink = document.querySelectorAll('.navigation-link'),
+	longGoodsList = document.querySelector('.long-goods-list'),
+	sectionTitle = document.querySelectorAll('.section-title')[1],
+	cardOne = document.querySelector('.card-1'),
+	cardTwo = document.querySelector('.card-2');
+
+const getGoods = async () => {
+		const result = await fetch('db/db.json');
+
+		if (!result.ok) {
+			throw console.error(`Ошибка ${res.status}!`);
+		}
+
+		return await result.json();
+	},
+	createCard = ({
+		id,
+		label,
+		img,
+		name,
+		description,
+		price
+	}) => {
+		const card = document.createElement('div');
+
+		card.className = 'col-lg-3 col-sm-6';
+
+		card.id = id;
+
+		card.innerHTML = `
+			<div class="goods-card">
+				${label ? `<span class="label">${label}</span>` : ''}
+				<img src="db/${img}" alt="image: ${name}" class="goods-image">
+				<h3 class="goods-title">${name}</h3>
+				<p class="goods-description">${description}</p>
+				<button class="button goods-card-btn add-to-cart" data-id="${id}">
+					<span class="button-price">$${price}</span>
+				</button>
+			</div>`;
+
+		return card;
+	},
+	renderCards = data => {
+		longGoodsList.textContent = '';
+
+		const cards = data.map(createCard);
+
+		longGoodsList.append(...cards);
+
+		document.body.classList.add('show-goods');
+	},
+	filterCards = (field, value) => {
+		getGoods()
+			.then(data => data.filter(good => good[field] === value))
+			.then(renderCards);
+	};
+
+more.addEventListener('click', e => {
+	e.preventDefault();
+
+	scrolling(e, more);
+
+	getGoods().then(renderCards);
+});
+
+navigationLink.forEach(link => link.addEventListener('click', e => {
+	e.preventDefault();
+
+	if (link.textContent === 'All') {
+		sectionTitle.textContent = 'All goods'
+		getGoods().then(renderCards);
+	} else {
+		sectionTitle.textContent = link.textContent;
+		filterCards(link.dataset.field, link.textContent);
+	}
+}));
+
+cardOne.addEventListener('click', e => {
+	if (e.target.closest('.button')) {
+		sectionTitle.textContent = 'Clothing';
+		filterCards('category', 'Clothing');
+	}
+})
+cardTwo.addEventListener('click', e => {
+	if (e.target.closest('.button')) {
+		sectionTitle.textContent = 'Accessories';
+		filterCards('category', 'Accessories');
+	}
+})
